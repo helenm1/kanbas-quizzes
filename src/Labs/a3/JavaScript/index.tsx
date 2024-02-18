4543ad2a22bf3dac2cf0bebf1f8679d6d62c1663
helenm1 <95582910+helenm1@users.noreply.github.com>
2024-02-18 00:07:04 -0500

    did up to 2.2.6 Working with functions

---
+++ src/Labs/a3/JavaScript/index.tsx
@@ -0,0 +1,19 @@
+import VariablesAndConstants from "./variables/VariablesAndConstants";
+import VariableTypes from "./variables/VariableTypes";
+import BooleanVariables from "./variables/BooleanVariables";
+import IfElse from "./conditionals/IfElse";
+import TernaryOperator from "./conditionals/TernaryOperator";
+function JavaScript() {
+  console.log("Hello World!");
+  return (
+    <div>
+      <h1>JavaScript</h1>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+    </div>
+  );
+}
+export default JavaScript;
