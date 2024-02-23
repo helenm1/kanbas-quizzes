4d679d2b7425de18f73524331f8d91f8ffac4fb9
helenm1 <95582910+helenm1@users.noreply.github.com>
2024-02-23 16:16:17 -0500

    test

--- src/Kanbas/navigation/index.tsx
+++ src/Kanbas/navigation/index.tsx
@@ -11,7 +11,7 @@ import {
   FaShareSquare,
   FaRegQuestionCircle,
 } from "react-icons/fa";
-import nLogo from "../../images/n-logo.png";
+// import nLogo from "../../images/n-logo.png";
 
 function KanbasNavigation() {
   const links = [
@@ -28,7 +28,7 @@ function KanbasNavigation() {
   const { pathname } = useLocation();
   return (
     <div>
-      <img src={nLogo} alt="nLogo" width="80" height="80"></img>
+      {/* <img src={nLogo} alt="nLogo" width="80" height="80"></img> */}
       <ul className="wd-kanbas-navigation">
         {links.map((link, index) => (
           <li
