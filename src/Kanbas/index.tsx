45c902c98f6908d5c03ad8fe0b2cdcb0ff17ed48
helenm1 <95582910+helenm1@users.noreply.github.com>
2024-02-23 15:53:14 -0500

    path fix 3

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,7 +1,7 @@
+import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
-import KanbasNavigation from "./Navigation";
 function Kanbas() {
   return (
     <div className="d-flex">
