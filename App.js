03402815c0a78fefd568912ec296d59614dabd9d
helenm1 <95582910+helenm1@users.noreply.github.com>
2024-03-30 22:06:22 -0400

    finishing 4.2, with axios bug

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
+CourseRoutes(app);
 app.use(express.json());
 Lab5(app);
 Hello(app);
